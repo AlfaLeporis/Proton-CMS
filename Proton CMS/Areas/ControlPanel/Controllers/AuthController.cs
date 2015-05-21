db4a8ee41ba88c2e255b62e52bc193523dbe816e
AlfaLeporis <alfaleporis@gmail.com>
2015-05-21 17:39:42 +0200

    Added control panel area

---
+++ Proton CMS/Areas/ControlPanel/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proton_CMS.Areas.ControlPanel.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: ControlPanel/Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
