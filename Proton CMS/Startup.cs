982a309c26ce594c2e086c3ee3d083feff6e73cc
AlfaLeporis <alfaleporis@gmail.com>
2015-05-21 15:09:21 +0200

    Added new VS project

---
+++ Proton CMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proton_CMS.Startup))]
+namespace Proton_CMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
