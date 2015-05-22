d20af8ccbccd3654ccc426b0bd1d0a7d8e60d329
AlfaLeporis <alfaleporis@gmail.com>
2015-05-22 13:13:36 +0200

    Updated DatabaseContext, added Proton settings model

--- Proton CMS/DAL/DatabaseContext.cs
+++ Proton CMS/DAL/DatabaseContext.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Proton_CMS.Models;
 
 namespace Proton_CMS.DAL
 {
     public class DatabaseContext : DbContext
     {
-       
+        public DbSet<ProtonConfigModel> ProtonConfig { get; set; }
+
+        public DatabaseContext() : base("MainConnection")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Proton CMS/Models/ProtonConfigModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proton_CMS.Models
+{
+    public class ProtonConfigModel
+    {
+        public String AdminName { get; set; }
+        public String AdminPassword { get; set; }
+        public String AdminEMail { get; set; }
+    }
+}
